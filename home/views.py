fcd73a0a6303b6f3047a54d4e21af8af8e65348d
Saurav <sp.gharti@gmail.com>
2018-05-07 17:25:33 +0000

    add package django-social-auth

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-social-auth', 'url': 'http://pypi.python.org/pypi/django-social-auth/0.7.28'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
 	{'name':'django-guardian', 'url': 'http://pypi.python.org/pypi/django-guardian/1.4.9'},
     ]
